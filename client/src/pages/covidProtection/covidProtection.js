e24e62e6052d14e2744c8691d33e005edfcb87ec
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2021-12-04 03:48:59 +0600

    Revert "Delete client/src/pages directory"
    This reverts commit 1bb95c7e1d89fc8dfcfd2d7314b5d61fb8bb8034.

---
+++ client/src/pages/covidProtection/covidProtection.js
@@ -0,0 +1,186 @@
+import "./covidProtection.css"
+
+import React from 'react'
+
+function CovidProtection() {
+    return (
+        <div className="protecc">
+            <header className="proteccBanner">
+                <h1>Protect Yourself Against COVID-19</h1>
+                <section className="proteccDef">
+                    <div className="proteccDefInfo">
+                        <h2>What is COVID-19?</h2>
+                        <p>
+                            COVID-19 (coronavirus disease 2019) is a disease caused by a virus named SARS-CoV-2 and was discovered in December 2019 in Wuhan, China. It is very contagious and has quickly spread around the world.
+
+                            COVID-19 most often causes respiratory symptoms that can feel much like a cold, a flu, or pneumonia. COVID-19 may attack more than your lungs and respiratory system. Other parts of your body may also be affected by the disease.
+                            <ul>
+                                <li>Most people with COVID-19 have mild symptoms, but some people become severely ill.</li>
+                                <li>Some people including those with minor or no symptoms may suffer from post-COVID conditions — or “long COVID”.</li>
+                                <li>Older adults and people who have certain underlying medical conditions are at increased risk of severe illness from COVID-19.</li>
+                                <li>(Accurate number) of people have died from COVID-19 in Bangladesh.</li>
+                                <li>Vaccines against COVID-19 are safe and effective. Vaccines teach our immune system to fight the virus that causes COVID-19.</li>
+                            </ul>
+                            Learn more at: <a href="https://www.cdc.gov/coronavirus/2019-ncov/your-health/about-covid-19/basics-covid-19.htmlhttps://www.cdc.gov/coronavirus/2019-ncov/your-health/about-covid-19/basics-covid-19.html">The CDC</a>
+                        </p>
+                    </div>
+                    <div className="proteccDefPhoto">
+                        <img src="" alt="" />
+                    </div>               
+                </section>
+        </header>
+
+        <main className="proteccSteps">
+            <h2>How To Protect Yourself & Others </h2>
+
+            <section className="oddStep">
+                <div className="oddStepInfo">
+                    <h3>Get Vaccinated</h3>
+                    <p>
+                        <ul>
+                            <li>Authorized COVID-19 vaccines can help protect you from COVID-19.</li>
+                            <li>You should get a COVID-19 vaccine as soon as you can. <a href="../register/register.js"> You can register on our website.</a></li>
+                            <li>Once you are fully vaccinated, you may be able to start doing some things that you had stopped doing because of the pandemic.</li>
+                        </ul>
+                    </p>
+                </div>
+                <div className="oddStepPhoto">
+                    <img src="" alt="" />
+                </div>                
+            </section>
+
+            <section className="evenStep">
+                <div className="evenStepInfo">
+                    <h3>Wear A Mask</h3>
+                    <p>
+                        <ul>
+                            <li>Everyone 2 years or older who is not fully vaccinated should wear a mask in indoor public places.</li>
+                            <li>People who have a condition or are taking medications that weaken their immune system may not be fully protected even if they are fully vaccinated. They should continue to take all precautions recommended for unvaccinated people, including wearing a well-fitted mask, until advised otherwise by their healthcare provider.</li>
+                            <li>If you are fully vaccinated, to maximize protection and prevent possibly spreading COVID-19 to others, wear a mask indoors in public if you are in an area of substantial or high transmission.</li>
+                        </ul>
+                    </p>
+                </div>
+                <div className="evenStepPhoto">
+                    <img src="" alt="" />
+                </div>
+            </section>
+
+            <section className="oddStep">
+                <div className="oddStepInfo">
+                    <h3>Stay 6 feet away from others</h3>
+                    <p>
+                        <ul>
+                            <li>Avoid close contact with people who are sick.</li>
+                            <li>If possible, maintain 6 feet between the person who is sick and other household members.</li>
+                        </ul>
+                    </p>
+                </div>
+                <div className="oddStepPhoto">
+                    <img src="" alt="" />
+                </div>                
+            </section>
+
+            <section className="evenStep">
+                <div className="evenStepInfo">
+                    <h3>Avoid crowds and poorly ventilated spaces</h3>
+                    <p>
+                        <ul>
+                            <li>Being in crowded places like restaurants, bars, fitness centers, or movie theaters puts you at higher risk for COVID-19.</li>
+                            <li>Avoid indoor spaces that do not offer fresh air from the outdoors as much as possible.</li>
+                            <li>If indoors, bring in fresh air by opening windows and doors, if possible.</li>
+                        </ul>
+                    </p>
+                </div>
+                <div className="evenStepPhoto">
+                    <img src="" alt="" />
+
+                </div>
+            </section>
+
+            <section className="oddStep">
+                <div className="oddStepInfo">
+                    <h3>Wash your hands often</h3>
+                    <p>
+                        <ul>
+                            <li>Wash your hands often with soap and water for at least 20 seconds especially after you have been in a public place, or after blowing your nose, coughing, or sneezing.</li>
+                            <li>It’s especially important to wash your hands:
+                                <ul> 
+                                    <li>Before eating or preparing food</li>
+                                    <li>Before touching your face</li>
+                                    <li>After using the restroom</li>
+                                    <li>After leaving a public place</li>
+                                    <li>After blowing your nose, coughing, or sneezing</li>
+                                    <li>After handling your mask</li>
+                                    <li>After changing a diaper</li>
+                                    <li>After caring for someone sick</li>
+                                    <li>After touching animals or pets</li>
+                                </ul>
+                            </li>
+                            <li>If soap and water are not readily available, use a hand sanitizer that contains at least 60% alcohol. Cover all surfaces of your hands and rub them together until they feel dry.</li>
+                            <li>Avoid touching your eyes, nose, and mouth with unwashed hands.</li>
+                        </ul>
+                    </p>
+                </div>
+                <div className="oddStepPhoto">
+                    <img src="" alt="" />
+                </div>                
+            </section>
+
+            <section className="evenStep">
+                <div className="evenStepInfo">
+                    <h3>Cover coughs and sneezes</h3>
+                    <p>
+                        <ul>
+                            <li>If you are wearing a mask: You can cough or sneeze into your mask. Put on a new, clean mask as soon as possible and wash your hands.</li>
+                            <li>If you are not wearing a mask:
+                                <ul>
+                                    <li>Always cover your mouth and nose with a tissue when you cough or sneeze, or use the inside of your elbow and do not spit.</li>
+                                    <li>Throw used tissues in the trash.</li>
+                                    <li>Immediately wash your hands with soap and water for at least 20 seconds. If soap and water are not readily available, clean your hands with a hand sanitizer that contains at least 60% alcohol.</li>
+                                </ul>
+
+                            </li>
+                        </ul>
+                    </p>
+                </div>
+                <div className="evenStepPhoto">
+                    <img src="" alt="" />
+
+                </div>
+            </section>
+
+            <section className="oddStep">
+                <div className="oddStepInfo">
+                    <h3>Monitor your health daily</h3>
+                    <p>
+                        <ul>
+                            
+                            <li>Be alert for symptoms:
+                                <ul> 
+                                    <li>Watch for fever, cough, shortness of breath, or other symptoms of COVID-19.</li>
+                                    <li>Take your temperature if symptoms develop.</li>
+                                    <li>Don’t take your temperature within 30 minutes of exercising or after taking medications that could lower your temperature, like acetaminophen.</li>
+                                </ul>
+                            </li>
+                            <li>Follow <a href="https://www.cdc.gov/coronavirus/2019-ncov/if-you-are-sick/steps-when-sick.html">CDC guidance</a> if symptoms develop.</li>
+                            
+                        </ul>
+                    </p>
+                </div>
+                <div className="oddStepPhoto">
+                    <img src="" alt="" />
+                </div>                
+            </section>
+
+
+
+
+        </main>
+
+        </div>
+        
+        
+    )
+}
+
+export default CovidProtection

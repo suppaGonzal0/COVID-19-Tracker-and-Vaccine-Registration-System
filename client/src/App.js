64dc0e71b3235b9cbfaae81a2b8563c70466e8c1
Samin Ahsan <turzo68@gmail.com>
2021-12-04 03:19:46 +0600

    Added skeleton for covid protection page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-
+import CovidProtection from './pages/covidProtection/CovidProtection';
 function App() {
   return (
     <div className="App">
+      <CovidProtection/>
     </div>
   );
 }
